fcedef8b021fdcf3e759aa7c3b88748440713030
菜菜子 <84311024+yqnnko@users.noreply.github.com>
2022-05-17 11:16:36 +0800

    Update bangumi.py

--- bangumi.py
+++ bangumi.py
@@ -20,35 +20,53 @@ def getLocalDirName(s: str):
 def updateCsvFile(torrent, torrentPath: str, csvPath: str = 'output.csv'):
     #读取种子文件 获取准确的文件数量和大小
     reader = bencoder.decode(open(torrentPath,'rb').read())
-    #获取CSV中的各列
+    count = size = 0
+    if b'files' in reader[b'info'].keys():
+        for file in reader[b'info'][b'files']:
+            size = size + file[b'length']
+            count = count + 1
+    else:
+        count = 1
+        size = reader[b'info'][b'length']
+    #从bangumi返回的torrent中读取其他列
     time = torrent['publish_time']
     title = getLocalDirName(torrent['title'])
     taskName = reader[b'info'][b'name'].decode()
-    count = size = 0
-    for file in reader[b'info'][b'files']:
-        size = size + file[b'length']
-        count = count + 1
     magnet = torrent['magnet']
     #写CSV行
     writer = csv.writer(open(csvPath,'a',encoding='utf-8',newline=''))
     writer.writerow([time[:10], title, taskName, count, size, magnet])
 
-#从bangumi.moe获取种子信息
-def getTorrentsFromBangumi(size: int, *keys: str):
-    page = 1
+#根据标签从bangumi.moe搜索种子信息
+def getTorrentsFromBangumiByTags(size: int, *tags: str):
     torrents = []
     url = 'https://bangumi.moe/api/torrent/search'
-    payload = {"tag_id": keys, "p": page}
-    while size > 0:
+    payload = {"tag_id": tags, "p": 1}
+    while size > len(torrents):
         res = requests.post(url, json=payload)
-        if page == 1:
+        if payload['p'] == 1:
             size = min(json.loads(res.text)['count'], size)
         torrents = torrents + json.loads(res.text)['torrents'][:size]
-        size = size - len(torrents)
+        payload['p'] = payload['p'] + 1
+    return torrents
+
+#根据关键字从bangumi.moe搜索种子信息
+def getTorrentsFromBangumiByKey(size: int, key: str):
+    torrents = []
+    url = 'https://bangumi.moe/api/v2/torrent/search'
+    payload = {"query": key, "p": 1}
+    while size > len(torrents):
+        res = requests.post(url, json=payload)
+        if payload['p'] == 1:
+            size = min(json.loads(res.text)['count'], size)
+        torrents = torrents + json.loads(res.text)['torrents'][:size]
+        payload['p'] = payload['p'] + 1
     return torrents
 
 #从bangumi.moe获取种子文件
 def downloadTorrentsFromBangumi(torrents: list, path: str = 'torrents'):
+    if not os.path.exists(path):
+        os.mkdir(path)
     url = 'https://bangumi.moe/download/torrent/'
     for torrent in torrents:
         torrentFileName = getBangumiFileName(torrent['title']) + '.torrent'
@@ -60,5 +78,10 @@ def downloadTorrentsFromBangumi(torrents: list, path: str = 'torrents'):
 
 
 if __name__ == '__main__':
-    torrents = getTorrentsFromBangumi(20, '5596b174a0b788232ee352cb')
+    size = int(input('请输入要抓取的种子数量 (默认 999): ') or 999)
+    tags = input('请输入标签tags,多个标签以逗号隔开 (默认 624774a470e8f89b23a1855e): ').split(',') or '624774a470e8f89b23a1855e'
+    key = input('请输入关键字 (默认 VCB): ') or 'VCB'
+
+    torrents = getTorrentsFromBangumiByTags(size, tags)
+    #torrents = getTorrentsFromBangumiByKey(size, key)
     downloadTorrentsFromBangumi(torrents, 'torrents')
